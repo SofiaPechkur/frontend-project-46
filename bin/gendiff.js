3633b4ef9659e59f7e20999bfecab025cf42e520
Sofiya <spechkur@mail.ru>
2024-10-25 17:48:49 +0300

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,11 @@ import { program } from 'commander';
 
 program
     .name('gendiff')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .description('Compares two configuration files and shows a difference.')
     .version('output the version number')
+    .option('-f, --format [type]', 'output format')
     .helpOption('-h, --help', 'output usage information');
 
 program.parse();
